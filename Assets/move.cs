19d7a30853a5bf9da776debcd409052b2b6c8bb2
skku-sleepwalk <lucas7211@g.skku.edu>
2023-10-21 19:19:23 +0900

    temp

--- Assets/move.cs
+++ Assets/move.cs
@@ -1,25 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class move : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float gravityPower;
-    //float jumping = 0f;
-    public float baseSpeed = 2f; // 기본 이동 속도
+    //public float gravityPower;
+
+    public float baseSpeed = 3f; // 기본 이동 속도
     public float accelerationRate = 0.1f; // 가속도 조절을 위한 상수
     private float timeElapsed = 0f;
     public Animator animator;
-    //public float jumpForce = 10f;  // 점프에 사용될 힘
-    //public float jumpDuration = 1f; // 점프 지속 시간 (초)
+    public float jumpForce = 5f;  // 점프에 사용될 힘
+    public float jumpDuration = 0.5f; // 점프 지속 시간 (초)
 
-    //private bool isJumping = false;
-    //private float jumpStartTime;
-    // Update is called once per frame
+    private bool isJumping = false;
+    private float jumpStartTime;
+    public bool isRight = true;
+
     void Update()
     {
-        transform.Translate(Vector3.down * gravityPower);
+        //transform.Translate(Vector3.down * gravityPower);
         if (Input.GetKey(KeyCode.RightArrow))
         {
             timeElapsed += Time.deltaTime;
@@ -46,28 +48,40 @@ public class move : MonoBehaviour
             animator.ResetTrigger("move");
             animator.SetTrigger("stop");
         }
-        //if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
-        //{
-        //    isJumping = true;
-        //    jumpStartTime = Time.time;
-        //}
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            jumpStartTime = Time.time;
+        }
 
-        //// 점프 중일 때
-        //if (isJumping)
-        //{
-        //    // 점프 지속 시간 동안 플레이어를 위로 움직임
-        //    float elapsedTime = Time.time - jumpStartTime;
-        //    if (elapsedTime < jumpDuration)
-        //    {
-        //        float jumpProgress = elapsedTime / jumpDuration;
-        //        float jumpHeight = Mathf.Lerp(0f, jumpForce, jumpProgress);
-        //        transform.Translate(Vector3.up * jumpHeight * Time.deltaTime);
-        //    }
-        //    else
-        //    {
-        //        // 점프가 끝났을 때
-        //        isJumping = false;
-        //    }
-        //}
+        // 점프 중일 때
+        if (isJumping)
+        {
+            animator.SetTrigger("jump");
+            animator.ResetTrigger("notJump");
+            // 점프 지속 시간 동안 플레이어를 위로 움직임
+            float elapsedTime = Time.time - jumpStartTime;
+            if (elapsedTime < jumpDuration)
+            {
+                
+                transform.Translate(Vector3.up * jumpForce * Time.deltaTime);
+            }
+
+
+        }
+        else
+        {
+            animator.SetTrigger("notJump");
+            animator.ResetTrigger("jump");
+
+        }
     }
-}
+
+       public void OnCollisionEnter2D(Collision2D collision) {
+            if (collision.gameObject.tag == "QBoxTop"|| collision.gameObject.tag == "BoxTop"|| collision.gameObject.tag == "Ground")
+            {
+                 isJumping = false;
+            }
+
+    }
+}   
